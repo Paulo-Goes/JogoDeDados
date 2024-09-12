5ce26cf4d285df2dfb9a7aa35df1c3a7a3802dcd
Paulo-Goes <paulo.2021104820@unicap.br>
2024-09-12 18:33:25 -0300

    Lógica principal feita

--- app/src/main/java/com/example/jogodedados/MainActivity.kt
+++ app/src/main/java/com/example/jogodedados/MainActivity.kt
@@ -6,10 +6,18 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.jogodedados.ui.theme.JogoDeDadosTheme
 
@@ -19,13 +27,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             JogoDeDadosTheme {
-                Dice()
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = Color.White
+                ) {
+                    Dice()
+                }
             }
         }
     }
 }
 
+@Composable
 fun Dice() {
+    var dice1 by remember { mutableIntStateOf(1) }
+    var dice2 by remember { mutableIntStateOf(1) }
+    var totalGames by remember { mutableIntStateOf(0) }
+    var totalWins by remember { mutableIntStateOf(0) }
+    var result by remember { mutableStateOf("") }
+
+    fun roll() {
+        dice1 = (1..6).random()
+        dice2 = (1..6).random()
+        totalGames++
+
+        if(dice1 + dice2 in setOf(7, 11)) {
+            totalWins++
+            result = "Ganhou"
+        } else {
+            result = "Perdeu"
+        }
+    }
+
+    val winPercentage = if (totalGames > 0) (totalWins * 100) / totalGames else 0
 
 }
 
