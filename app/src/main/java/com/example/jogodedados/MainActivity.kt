62f1976fe2b19a82283c670427963c68bb33a2a9
Paulo-Goes <paulo.2021104820@unicap.br>
2024-09-12 18:57:29 -0300

    Optimize imports

--- app/src/main/java/com/example/jogodedados/MainActivity.kt
+++ app/src/main/java/com/example/jogodedados/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.jogodedados
 
 import android.os.Bundle
-import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,13 +8,9 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
